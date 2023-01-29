dea84744d9a2fb22faa1684a414cd6398b14d144
r-zeeshan <111675443+r-zeeshan@users.noreply.github.com>
2023-01-29 22:30:25 +0500

    update

--- utils.py
+++ utils.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 
+def getName(filePath):
+    return filePath.split("\\")[-1]
 
 
 def importDataInfo(path):
     columns = ["Center", "Left", "Right", "Steering", "Throttle", "Brake", "Speed"]
     data = pd.read_csv(os.path.join(path, "driving_log.csv"), names=columns)
+    data["Center"] = data["Center"].apply(getName)
     print(data.head())
+    print("Total images Imported: " , data.shape[0])
+    return data
+
 
