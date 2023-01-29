7de4e7b1ab7f62f0d80978ac29a3c5ee63d15810
r-zeeshan <111675443+r-zeeshan@users.noreply.github.com>
2023-01-29 22:04:06 +0500

    update

--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 
+
+
 def importDataInfo(path):
     columns = ["Center", "Left", "Right", "Steering", "Throttle", "Brake", "Speed"]
-    # data = pd.read_csv(os.path.join(path, "driving_log.csv"), names=columns)
-    data = pd.read_csv("/content/self-driving-car/SimulationData/driving_log.csv", names=columns)
+    data = pd.read_csv(os.path.join(path, "driving_log.csv"), names=columns)
     print(data.head())
+
