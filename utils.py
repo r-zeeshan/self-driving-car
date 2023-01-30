e8b489ed8c4e6d66cb1a3841d15103ad1a2f705c
r-zeeshan <111675443+r-zeeshan@users.noreply.github.com>
2023-01-30 12:22:05 +0500

    update

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 def getName(filePath):
@@ -14,4 +15,12 @@ def importDataInfo(path):
     print("Total images Imported: " , data.shape[0])
     return data
 
+
+def balanceData(data, display=True):
+    nBins = 31
+    samplesPerBin = 500
+    hist, bins = np.histogram(data["Steering"], nBins)
+    center = (bins[:-1] + bins[1:]) * 0.5
+    plt.bar(center, hist, width=0.06)
+    plt.show()
 
